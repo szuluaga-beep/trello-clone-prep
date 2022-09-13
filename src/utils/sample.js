c826831efbae2ecfc034c3f1fdcea30dee662041
Steven Zuluaga <steven000069@hotmail.com>
2022-09-12 23:40:49 -0500

    creando la data de ejemplo

---
+++ src/utils/sample.js
@@ -0,0 +1,27 @@
+const data = {
+     lists: [
+          {
+               id: "list-1",
+               title: "To do",
+               cards: [
+                    {
+                         id: "1",
+                         title:"Aprender HTML"
+                    }
+               ]
+          },
+          {
+               id: "list-2",
+               title: "In progress",
+               cards: []
+          },
+          {
+               id: "list-3",
+               title: "Completed",
+               cards:[]
+          }
+     ],
+     listIds:["list-1","list-2","list-3"]
+}
+
+export default data
