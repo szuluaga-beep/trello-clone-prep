7a1d896a28a69413cd2b909f2a0fba548fb198a5
Steven Zuluaga Cortes <steven000069@hotmail.com>
2022-09-12 21:00:52 -0500

    Agregando los componentes Home y Navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import "./styles.css"
+
+const Navbar = () => {
+  return (
+    <div>
+        <p className='menu'>Navbar</p>
+    </div>
+  )
+}
+
+export default Navbar
