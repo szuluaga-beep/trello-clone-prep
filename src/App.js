7940f7e33b9b46521c1e95c09daf6cc1c0e341ca
Steven Zuluaga Cortes <steven000069@hotmail.com>
2022-09-12 19:39:31 -0500

    Instalación de Firebase

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
+
 const App = () => {
   return (
     <div>
-      <p>lorem</p>
+      <p>Hello World</p>
     </div>
   )
 }
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCIoaUWTzlJmcsgq9G0rm6Ih5oi2VcaNT4",
+  authDomain: "trello-clone-2169b.firebaseapp.com",
+  projectId: "trello-clone-2169b",
+  storageBucket: "trello-clone-2169b.appspot.com",
+  messagingSenderId: "879480724153",
+  appId: "1:879480724153:web:f281d44e16c8c1447b1968",
+};
